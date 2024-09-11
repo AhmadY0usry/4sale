2e9bfd0ba4d43250e538be4c05f1242f6fdbf860
AhmadY0usry <a.yosry344@gmail.com>
2024-09-11 17:44:27 +0300

    Adding TestNG.xml file

--- src/main/java/Pages/Checkout_Page.java
+++ src/main/java/Pages/Checkout_Page.java
@@ -6,31 +6,29 @@ import org.openqa.selenium.WebDriver;
 
 public class Checkout_Page extends Page_Utils {
 
-WebDriver driver;
-By firstName = By.id("first-name");
-By lastName = By.id("last-name");
-By zipCode = By.id("postal-code");
-By Continue = By.id("continue");
-By finish = By.id("finish");
+    WebDriver driver;
+    By firstName = By.id("first-name");
+    By lastName = By.id("last-name");
+    By zipCode = By.id("postal-code");
+    By Continue = By.id("continue");
+    By finish = By.id("finish");
+
     public Checkout_Page(WebDriver driver) {
         super(driver);
-        this.driver=driver;
+        this.driver = driver;
     }
 
-    public void enterBuyerInformation (String Name, String Last, String zipCode)
-    {
-        clearAndSendText(firstName,Name);
-        clearAndSendText(lastName,Last);
-        clearAndSendText(this.zipCode,zipCode);
+    public void enterBuyerInformation(String Name, String Last, String zipCode) {
+        clearAndSendText(firstName, Name);
+        clearAndSendText(lastName, Last);
+        clearAndSendText(this.zipCode, zipCode);
     }
 
-    public void clkOnContinueBtn()
-    {
+    public void clkOnContinueBtn() {
         click(Continue);
     }
 
-    public WebDriver clickOnFinishBtn()
-    {
+    public WebDriver clickOnFinishBtn() {
         click(finish);
         return driver;
     }
